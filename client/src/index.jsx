a742e3733f8934381b6b9f45d29aad45248dc6f4
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-29 22:34:55 +0530

    started development server and setup the project from the scratch

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 const App = () => {
- return <h1>This is my React app!</h1>;
+ return (
+ <div>
+
+ <h1>This is my React app!</h1>
+    <p>Hey there you did it.</p>
+ </div>
+    );
  }
 ReactDOM.render(<App />, document.getElementById('root'));

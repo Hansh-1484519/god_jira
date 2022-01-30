41e5c4124abea58026e73a83212d0ec93ca597a2
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-30 20:42:30 +0530

    updated index.jsx file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,12 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
+import ReactDOM from "react-dom";
+import React from "react";
 const App = () => {
- return (
- <div>
-
- <h1>This is my React app!</h1>
-    <p>Hey there you did it.</p>
- </div>
-    );
- }
-ReactDOM.render(<App />, document.getElementById('root'));
+  return (
+    <div>
+      <h1>This is my React app!</h1>
+      <p>Hey there you did it.</p>
+      <h2>good to seet you</h2>
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("root"));
